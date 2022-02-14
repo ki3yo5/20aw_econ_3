cb3a8613069d7e52b979773ab04eb5a800c13931
ki3yo5 <i.kiyotaka@gmail.com>
2022-02-14 16:23:20 +0900

    first commit of the project.

---
+++ 20aw_econ_analysis_3.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+library(readxl)
+library(dplyr)
+hh_exp <- read_excel("C:/Users/ikiyo/iCloudDrive/UTokyo_2020/20aw_econ_ta/3/hh_exp.xlsx")
+View(hh_exp)
+df <- filter( hh_exp , food_exp >= 5 )
+View(df)
